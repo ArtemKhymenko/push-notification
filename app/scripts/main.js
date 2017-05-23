b37203516678d9c2ee76bacb89394620479b98e8
Artem Khymenko <breakerhim@ua.fm>
2017-05-23 16:22:54 +0300

    Initialise State

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = '<BCaszWOu0QXyfSltlRoDRivbS5gyx792vSoP2s-ajOxCv4sRmhwzPuNVLiungrot9Ll5SSGPOQkinuNTyMsyrz0>';
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -58,3 +58,34 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.warn('Push messaging is not supported');
   pushButton.textContent = 'Push Not Supported';
 }
+function initialiseUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+navigator.serviceWorker.register('sw.js')
+.then(function(swReg) {
+  console.log('Service Worker is registered', swReg);
+
+  swRegistration = swReg;
+  initialiseUI();
+})
